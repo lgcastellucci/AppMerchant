8ea0d295d2da1ba51ede12e7ae86c8d85ca41031
lgcastellucci <castellucci@gmail.com>
2026-02-04 17:46:52 -0300

    Adicionando constantes para produção e testes

--- Publicar.bat
+++ Publicar.bat
@@ -1,5 +1,11 @@
 @echo off
 
+call ng build --configuration testing --base-href /hml-app_merchant-ng/ --output-path dist/hml-app_merchant-ng
+
+robocopy /MIR dist\hml-app_merchant-ng\browser w:\NginxCas_data\_data\hml-app_merchant-ng\
+
+rmdir /S/Q dist
+
 call ng build --configuration production --base-href /app_merchant-ng/ --output-path dist/app_merchant-ng
 
 robocopy /MIR dist\app_merchant-ng\browser w:\NginxCas_data\_data\app_merchant-ng\
---
+++ src/app/app.constants.prod.ts
@@ -0,0 +1,4 @@
+export class AppConstants {
+  static UrlBase = "https://transacoes.siscred.com.br/api/v1/";
+  static ChaveIntegrador = "";
+}
